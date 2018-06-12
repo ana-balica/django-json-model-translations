1bd921b4def3b736e9aa865b78195c0780a30de0
Ana Balica <ana.balica@getstreetteam.com>
2018-06-12 08:32:28 +0100

    Drop base_field kwarg as it's mandatory and make it an arg

--- djjmt/fields.py
+++ djjmt/fields.py
@@ -3,10 +3,7 @@ from django.db import models
 
 class TranslationJSONField(models.JSONField):
 
-    def __init__(self, verbose_name=None, name=None, encoder=None, **kwargs):
-        super().__init__(verbose_name, name, encoder, **kwargs)
-        self.base_field = kwargs.get('base_field')
-        self.langs = kwargs.get('langs', [])
-
-        if not self.base_field:
-            raise ValueError('The base_field parameter must be provided.')
+    def __init__(self, base_field, langs=None, **kwargs):
+        super().__init__(**kwargs)
+        self.base_field = base_field
+        self.langs = langs or []

311454e7e3b4dd8513c3195ed242e1538da49081
Ana Balica <ana.balica@getstreetteam.com>
2018-06-13 20:32:19 +0100

    Fix import for JSONField

--- djjmt/fields.py
+++ djjmt/fields.py
@@ -1,8 +1,8 @@
-from django.db import models
+from django.contrib.postgres.fields import JSONField
 from django.utils.translation import gettext_lazy as _
 
 
-class TranslationJSONField(models.JSONField):
+class TranslationJSONField(JSONField):
     description = _('A JSON object with translations')
 
     def __init__(self, base_field, langs=None, **kwargs):

087ba0cddcdf31f5cc6aaae217b1e1cf609c0ac4
Ana Balica <ana.balica@getstreetteam.com>
2018-06-18 09:24:50 +0100

    Add from_db_value to return the value in active lang

--- djjmt/fields.py
+++ djjmt/fields.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.contrib.postgres.fields import JSONField
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import get_language
 from django.utils.translation import gettext_lazy as _
 
 
@@ -16,3 +19,19 @@ class TranslationJSONField(JSONField):
         if self.langs is not None:
             kwargs['langs'] = self.langs
         return name, path, args, kwargs
+
+    def from_db_value(self, value, expression, connection):
+        # TODO: convert to a func that's called when the value is read, not when it's loaded from db
+        # TODO: normalize the language code to a common format
+        lang = get_language()
+        if lang is None:
+            raise ImproperlyConfigured('Enable translations to use TranslationJSONField.')
+
+        if value is None:
+            return value
+
+        if lang in value:
+            return value.get(lang)
+        else:
+            default_lang = settings.LANGUAGE_CODE
+            return value.get(default_lang)

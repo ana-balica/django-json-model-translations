6a800750f5d06ac35772732a59968109f12e8bb9
Ana Balica <ana.balica@getstreetteam.com>
2018-07-22 09:14:15 +0100

    Add tests for descriptor setter

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import pytest
 
 from django.core.exceptions import ImproperlyConfigured
@@ -19,7 +20,7 @@ def test_get_default_translation(field_descriptor):
         assert field_descriptor.__get__(None, None) == 'hello'
 
 
-def test_get_translation_based_on_active_language(field_descriptor):
+def test_get_translation_by_lang(field_descriptor):
     with override('fr_FR'):
         assert field_descriptor.__get__(None, None) == 'salut'
 
@@ -31,8 +32,41 @@ def test_get_missing_translation_for_default_lang(field_descriptor):
 
 
 @override_settings(LANGUAGE_CODE=None)
-def test_get_translation_when_no_language(field_descriptor):
+def test_get_translation_when_no_lang(field_descriptor):
     with pytest.raises(ImproperlyConfigured) as exc:
         field_descriptor.__get__(None, None)
+
+    assert 'Enable translations to use TranslationJSONField.' == str(exc.value)
+
+
+def test_set_translation_json():
+    field_descriptor = TranslationJSONFieldDescriptor(field_name='foo')
+    assert field_descriptor.json_value is None
+
+    field_descriptor.__set__(None, {})
+    assert field_descriptor.json_value == {}
+
+    field_descriptor.__set__(None, {'en': 'Foo', 'de': 'Bar'})
+    assert field_descriptor.json_value == {'en': 'Foo', 'de': 'Bar'}
+
+
+def test_set_translated_value_by_lang(field_descriptor):
+    with override('fr_FR'):
+        field_descriptor.__set__(None, 'New french value')
+        assert field_descriptor.__get__(None, None) == 'New french value'
+        assert field_descriptor.json_value == {'en-gb': 'hello', 'fr-fr': 'New french value'}
+
+
+def test_set_translated_value_for_new_lang(field_descriptor):
+    with override('ru-RU'):
+        field_descriptor.__set__(None, 'привет')
+        assert field_descriptor.__get__(None, None) == 'привет'
+        assert field_descriptor.json_value == {'en-gb': 'hello', 'fr-fr': 'salut', 'ru-ru': 'привет'}
+
+
+@override_settings(LANGUAGE_CODE=None)
+def test_set_translation_when_no_lang(field_descriptor):
+    with pytest.raises(ImproperlyConfigured) as exc:
+        field_descriptor.__set__(None, 'value')
 
     assert 'Enable translations to use TranslationJSONField.' == str(exc.value)
